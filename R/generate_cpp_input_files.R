6e8eae5c53dd8c150e9d0381a7c6e6024cbf1dbd
Lucy Li <mengqi.li09@imperial.ac.uk>
2016-07-26 11:25:48 +0100

    function to create MCMC and params lists

--- R/generate_cpp_input_files.R
+++ R/generate_cpp_input_files.R
@@ -51,6 +51,55 @@ generate_cpp_input_files <- function(dt, params, mcmc_options, initial_states, d
   return (commandline.command)
 }
 
+create_mcmc_options <- function (particles=1000, iterations=1000, log_every=1, pfilter_every=20, which_likelihood=0, pfilter_threshold=1.0,
+                                 log_filename="log.txt", traj_filename="traj.txt") {
+  mcmc_options <- c(particles=particles, iterations=iterations, log_every=log_every,
+                    pfilter_every=pfilter_every, which_likelihood=which_likelihood,
+                    pfilter_threshold=pfilter_threshold, log_filename=log_filename,
+                    traj_filename=traj_filename)
+  return (mcmc_options)
+}
+
+
+create_params_list <- function (param_names=c("param"), init_param_values=c(1),
+                                params_to_estimate=c("param"), transform=c(NA),
+                                prior=c("unif"), prior_params=list(c(0.0, 0.1)),
+                                proposal=NULL, proposal_params=list(c(0.1, 0.0, 1.0)),
+                                optimal_acceptance=0.234, lower_acceptance=0.1, upper_acceptance=0.8,
+                                adapt_every=20, max_adapt_times=100) {
+  estimate <- param_names %in% params_to_estimate
+  num_params <- length(param_names)
+  input_params1 <- list(c(total_params=num_params, optimal_acceptance=optimal_acceptance,
+                         lower_acceptance=lower_acceptance, upper_acceptance=upper_acceptance,
+                         adapt_every=adapt_every, max_adapt_times=max_adapt_times))
+  input_params2 <- data.frame(matrix(nrow=num_params, ncol=12))
+  names(input_params2) <- c("param_value", "param_name", "estimate", "transform",
+                            "prior", "prior_param1", "prior_param2", "prior_param3",
+                            "proposal", "proposal_sd", "proposal_min", "proposal_max"
+                            )
+  input_params2$param_value <- init_param_values
+  input_params2$param_name <- param_names
+  input_params2$param_to_estimate <- tolower(estimate)
+  input_params2$prior <- NA
+  input_params2$prior[estimate] <- prior
+  input_params2$proposal <- NA
+  if (is.null(proposal)) input_params2$proposal[estimate] <- "normal"
+  a <- lapply(1:length(prior_params), function (i) {
+    input_params2[which(estimate)[i], "prior_param1"] <<- prior_params[[i]][1]
+    if (length(prior_params) > 1) input_params2[which(estimate)[i], "prior_param2"] <<- prior_params[[i]][2]
+    if (length(prior_params) > 2) input_params2[which(estimate)[i], "prior_param3"] <<- prior_params[[i]][3]
+    input_params2$proposal_sd[which(estimate)[i]] <<- proposal_params[[i]][1]
+    input_params2$proposal_min[which(estimate)[i]] <<- proposal_params[[i]][2]
+    input_params2$proposal_max[which(estimate)[i]] <<- proposal_params[[i]][3]
+  })
+  input_params2$prior_param1[!estimate] <- input_params2$prior_param2[!estimate] <-
+    input_params2$prior_param3[!estimate] <- input_params2$proposal_sd[!estimate] <-
+    input_params2$proposal_min[!estimate] <- input_params2$proposal_max[!estimate] <- NA
+  params_list <- c(input_params1, lapply(1:nrow(input_params2), function (i) input_params2[i, ]))
+  return(params_list)
+}
+
+
 #' Run the compile C++ program with the given input
 #'
 #' @param program
