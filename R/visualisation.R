c4e8c8e3636d869b07c758b3bbff09bd2ead60dc
Lucy Li <mengqi.li09@imperial.ac.uk>
2016-10-17 11:38:01 +0100

    a function to plot MCMC traces

--- R/visualisation.R
+++ R/visualisation.R
@@ -11,3 +11,27 @@ extract_gglegend <- function (Plot) {
   legend.plot <- legend.tmp$grobs[[which("guide-box" == sapply(legend.tmp$grobs, `[[`, "name"))]]
   return (legend.plot)
 }
+
+#' Plot the MCMC trace using the results data frame
+#'
+#' @param x data frame where each row contains the parameter values at a particular MCMC iteration
+#' @param burnin If not NULL, the proportion of the chain to discard.
+#' @param excludes A vector of character. Column/variable names in the data frame x to exclude from visualisation
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot.mcmc.trace <- function (x, burnin=NULL, excludes=NULL) {
+  if (!is.null(burnin)) {
+    x <- x[round(nrow(x)*burnin):nrow(x), ]
+  }
+  if (!is.null(excludes)) {
+    y <- x[, !(names(x) %in% excludes)]
+  }
+  y$iteration <- 1:nrow(y)
+  y <- reshape2::melt(y, id.vars="iteration")
+  require(ggplot2)
+  ggplot(y) + geom_line(aes(x=iteration, y=value)) +
+    facet_grid(variable~., scales="free_y")
+}
