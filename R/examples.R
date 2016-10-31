968cdf41295802d5a867c9a3b833dfbbf18cff7c
Lucy Li <mengqi.li09@imperial.ac.uk>
2016-10-31 11:12:59 +0000

    replace . with _

--- R/examples.R
+++ R/examples.R
@@ -4,7 +4,7 @@
 #' @export
 #'
 #' @examples
-example.sim.sir <- function () {
+example_sim_sir <- function () {
   simulate_sir(c(R0=2, Tg=5, k=1, N=5000, S=4999, I=1), dt=0.1,
                max_num_dt=1000, min_epi_size=20, maximum_attempts=1,
                track_transmissions=TRUE)
