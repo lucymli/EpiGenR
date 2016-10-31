511ea1cd29c06583624e0929f3d611c20f467d01
Lucy Li <mengqi.li09@imperial.ac.uk>
2016-10-31 11:12:22 +0000

    set track transmissions to TRUE

--- R/examples.R
+++ R/examples.R
@@ -5,5 +5,7 @@
 #'
 #' @examples
 example.sim.sir <- function () {
-  simulate_sir(c(R0=2, Tg=5, k=1, N=5000, S=4999, I=1), 0.1, 1000, 20, 1, FALSE)
+  simulate_sir(c(R0=2, Tg=5, k=1, N=5000, S=4999, I=1), dt=0.1,
+               max_num_dt=1000, min_epi_size=20, maximum_attempts=1,
+               track_transmissions=TRUE)
 }
