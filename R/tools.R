87efc7abc4839c26b4721c182ea9d0824e82239a
Lucy Li <mengqi.li09@imperial.ac.uk>
2017-04-19 16:56:46 -0400

    paste0 -> paste

--- R/tools.R
+++ R/tools.R
@@ -87,7 +87,7 @@ str_to_sentence <- function (strings) {
   if (length(strings)==1) return (strings)
   if (length(strings)==2) return (paste(strings, collapse=" and "))
   part1 <- paste(strings[1:(length(strings)-1)], collapse=", ")
-  return(paste0(part1, tail(strings, 1), sep=", and "))
+  return(paste(part1, tail(strings, 1), sep=", and "))
 }
 
 values_ci_to_str <- function (values, sep=", ") {
